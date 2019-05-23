28e971e421bbb9483d188ec836899218c87cc575
GabrielElkadiki <46852673+GabrielElkadiki@users.noreply.github.com>
2019-05-23 10:14:23 -0400

    Streamlined project for version control
    Use build.tcl scripts to build projects for use.

--- Stopwatch_Lite.ip_user_files/sim_scripts/clk_wiz/ies/run.f
+++
@@ -1,8 +0,0 @@
--makelib ies/xil_defaultlib \
-  "../../../../Arty_A7.srcs/sources_1/ip/clk_wiz/clk_wiz_clk_wiz.v" \
-  "../../../../Arty_A7.srcs/sources_1/ip/clk_wiz/clk_wiz.v" \
--endlib
--makelib ies/xil_defaultlib \
-  glbl.v
--endlib
-
